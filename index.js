da29514cdcf1f9c6e9c7981a31e29abcfb71e1fe
Iuoras Razvan <iuorasrazvan2003@yahoo.com>
2016-05-16 15:42:35 +0300

    commit

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ raznpm1 =require('raznpm1');
 output=raznpm1.printMsg(); 
 console.log(output); 
 
+exports.printMsg=function () {
+	
+	console.log("This is a message from ircnpm4 version 1.0.4"); 
+	
+}
+
